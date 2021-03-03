c275a28e100d02881d41df63aea202ad01aac15c
Vlad Abadzhiev <TheCryru@gmail.com>
2021-03-03 14:35:06 +0200

    Initial Commit

---
+++ FolderTwo/BlazorExecutable/BlazorLibrarySameFolder/Program.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorLibrarySameFolder
+{
+    public class Program
+    {
+
+    }
+}
